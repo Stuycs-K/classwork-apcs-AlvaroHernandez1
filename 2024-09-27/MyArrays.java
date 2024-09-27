bba4856d71136d9f2450edb88209a8aea89d5344
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-09-27 10:43:21 -0500

    Accounted for an empty array and added test cases

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -3,9 +3,12 @@ public class MyArrays{
     System.out.println("Expected: [1, 2, 3, 4, 5, 6, 0, 2147483647] Received: " + aryToString(new int[] {1, 2, 3, 4, 5, 6, 0, 2147483647}));
     System.out.println("Expected: [-2147483646 , -10, 0, -3] Received: " + aryToString(new int[] {-2147483646 , -10, 0, -3}));
     System.out.println("Expected: [0, 0, 0, 0, 0, 0] Received: " + aryToString(new int[6]));
+    System.out.println("Expected: [] Received: " + aryToString(new int[0]));
   }
 
   public static String aryToString(int[] nums){
+    if (nums.length == 0)
+      return "[]";
     String result = "[";
     for (int i = 0; i < nums.length - 1; i++)
     {
