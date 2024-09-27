9309c8977d0266fc1138230aca95dcddd13efffb
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-09-27 10:48:03 -0500

    Period ends, in the middle of testing returnCopy

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -1,9 +1,16 @@
 public class MyArrays{
   public static void main(String[] args){
+    // tests aryToString
     System.out.println("Expected: [1, 2, 3, 4, 5, 6, 0, 2147483647] Received: " + aryToString(new int[] {1, 2, 3, 4, 5, 6, 0, 2147483647}));
     System.out.println("Expected: [-2147483646 , -10, 0, -3] Received: " + aryToString(new int[] {-2147483646 , -10, 0, -3}));
     System.out.println("Expected: [0, 0, 0, 0, 0, 0] Received: " + aryToString(new int[6]));
     System.out.println("Expected: [] Received: " + aryToString(new int[0]));
+    System.out.println();
+    // tests returnCopy
+    System.out.println("Expected: [1, 2, 3, 4, 5, 6, 0, 2147483647] Received: " + returnCopy(new int[] {1, 2, 3, 4, 5, 6, 0, 2147483647}));
+    System.out.println("Address 1: " + (new int][1, 2, 3, 4, 5]) + "Address 2: " + (returnCopy ))
+
+    // tests concatArray
   }
 
   public static String aryToString(int[] nums){
