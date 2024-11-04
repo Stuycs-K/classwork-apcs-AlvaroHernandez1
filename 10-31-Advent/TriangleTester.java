665fe05690acca785a8730c960ed92bbf8ab9021
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-04 11:29:01 -0500

    Worked on Part A

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -5,13 +5,24 @@ import java.util.Scanner;
 
 public class ReadFile {
 
-  public static void main(String[] args) {
+  public static int countTrianglesA(String filename){
     //2 Opening a file requires a try/catch
     try {
-      File file = new File("inputTri.txt");//1
+      File file = new File(filename);//1
       Scanner input = new Scanner(file);
       //CODE THAT SCANS THE FILE.
+      int count = 0;
+      while (file.hasNextLine()){
+        String line = file.nextLine;
 
+        Integer s1 = line.nextInt();
+        Integer s2 = line.nextInt();
+        Integer s3 = line.nextInt();
+
+        if (isTriangle(s1, s2, s3)
+          count++;
+
+        }
 
       }
 
@@ -22,5 +33,9 @@ public class ReadFile {
       System.out.println("File not found");
       return; //you can return from a void function just don't put a value.
     }
+  }
+
+  public static boolean isTriangle(int s1, int s2, int s3){
+    return(!((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)))
   }
 }
