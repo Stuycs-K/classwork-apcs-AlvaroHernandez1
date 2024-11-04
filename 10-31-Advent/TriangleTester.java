298092927d2456fbc13e9561a1d78b8e2f25a623
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-04 11:36:26 -0500

    Added the testing file now need to go back and fix bugs

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -3,7 +3,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class ReadFile {
+public class TriangleTester {
+  public static void main(String[] args) {
+    System.out.println(countTrianglesA("inputTri.txt"));
+  }
 
   public static int countTrianglesA(String filename){
     //2 Opening a file requires a try/catch
@@ -15,27 +18,26 @@ public class ReadFile {
       while (file.hasNextLine()){
         String line = file.nextLine;
 
-        Integer s1 = line.nextInt();
-        Integer s2 = line.nextInt();
-        Integer s3 = line.nextInt();
+        int s1 = line.nextInt();
+        int s2 = line.nextInt();
+        int s3 = line.nextInt();
 
-        if (isTriangle(s1, s2, s3)
-          count++;
-
-        }
+        if (isTriangle(s1, s2, s3))
+          count ++;
 
       }
 
       input.close();//releases the file from your program
+      return count;
 
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
       System.out.println("File not found");
-      return; //you can return from a void function just don't put a value.
+      return 0; //you can return from a void function just don't put a value.
     }
   }
 
   public static boolean isTriangle(int s1, int s2, int s3){
-    return(!((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)))
+    return(!((s1 + s2 <= s3) || (s1 + s3 <= s2) || (s2 + s3 <= s1)));
   }
 }
