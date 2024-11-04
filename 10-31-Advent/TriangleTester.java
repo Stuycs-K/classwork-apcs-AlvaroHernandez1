b43e7aab0ca1d1faaf89f09045bb73756bbfc4c3
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-04 11:44:17 -0500

    Finished Part B

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class TriangleTester {
   public static void main(String[] args) {
-    System.out.println(countTrianglesA("inputTri.txt"));
+    System.out.println(countTrianglesA("inputTriA.txt"));
+    System.out.println(countTrianglesB("inputTri.txt"));
   }
 
   public static int countTrianglesA(String filename){
@@ -23,6 +24,42 @@ public class TriangleTester {
         if (isTriangle(s1, s2, s3))
           count ++;
 
+      }
+
+      input.close();//releases the file from your program
+      return count;
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return 0; //you can return from a void function just don't put a value.
+    }
+  }
+
+  public static int countTrianglesB(String filename){
+    //2 Opening a file requires a try/catch
+    try {
+      File file = new File(filename);//1
+      Scanner input = new Scanner(file);
+      //CODE THAT SCANS THE FILE.
+      int count = 0;
+      while (input.hasNextInt()){
+        int s11 = input.nextInt();
+        int s12 = input.nextInt();
+        int s13 = input.nextInt();
+        int s21 = input.nextInt();
+        int s22 = input.nextInt();
+        int s23 = input.nextInt();
+        int s31 = input.nextInt();
+        int s32 = input.nextInt();
+        int s33 = input.nextInt();
+
+        if (isTriangle(s11, s21, s31))
+          count ++;
+        if (isTriangle(s12, s22, s32))
+            count ++;
+        if (isTriangle(s13, s23, s33))
+          count ++;
       }
 
       input.close();//releases the file from your program
