81973c310b14cede60cf6431c841218df891d00c
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-07 11:31:07 -0500

    steps 7, 8: getname override, comment

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,3 +1,6 @@
+/*
+The Bird class's getName function was overridden to take the superclass function and alter it by inserting "The Mighty" in front of the original output
+*/
 public class Driver {
   public static void main (String[] args) {
     Animal animal1 = new Animal("Bark", 2, "Theo");
@@ -6,6 +9,6 @@ public class Driver {
     Bird bird1 = new Bird("Tweet", 1, "Polly", 6, "red");
     bird1.speak();
 
-    
+
   }
 }
