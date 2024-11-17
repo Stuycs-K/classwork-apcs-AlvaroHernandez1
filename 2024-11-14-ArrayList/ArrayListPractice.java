256c810ee7e41622436378f24873055336c37cab
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-16 23:14:41 -0500

    Fixed mix lists method and tested it

--- 2024-11-14-ArrayList/ArrayListPractice.java
+++ 2024-11-14-ArrayList/ArrayListPractice.java
@@ -24,43 +24,52 @@ public class ArrayListPractice{
   }
 
   public static ArrayList<String> makeReversedList(ArrayList<String> original){
-  //return a new ArrayList that is in the reversed order of the original.
-  ArrayList<String> returnArray = new ArrayList<String> (original.size());
-  for (int i = original.size(); i > 0; i--) {
-    returnArray.add(original.get(i - 1));
-  }
-  return returnArray;
-  }
-
-  public static ArrayList<String> mixLists( ArrayList<String> a,  ArrayList<String> b){
-  //return a new ArrayList that has all values of a and b in alternating order that is:
-  //a[0], b[0], a[1], b[1]...
-  //If one list is longer than the other, just attach the remaining values to the end.
-  ArrayList<String> returnArray = new ArrayList<String> (a.size() + b.size());
-  if (a.size() >= b.size()){
-    for (int i = 0; i < b.size(); i++){
-      returnArray.add(a.get(0));
-      returnArray.add(b.get(i));
-      a.remove(0);
+    //return a new ArrayList that is in the reversed order of the original.
+    ArrayList<String> returnArray = new ArrayList<String> (original.size());
+    for (int i = original.size(); i > 0; i--) {
+      returnArray.add(original.get(i - 1));
     }
-    for (int i = 0; i < a.size(); i++){
-      returnArray.add(a.get(i));
-    }
-
     return returnArray;
   }
-  else{
-    for (int i = 0; i < a.size(); i++){
-      returnArray.add(a.get(i));
-      returnArray.add(b.get(0));
-      b.remove(1);
+
+  public static ArrayList<String> mixLists(ArrayList<String> tempA,  ArrayList<String> tempB){
+    ArrayList<String> a = new ArrayList<String> (tempA.size());
+    ArrayList<String> b = new ArrayList<String> (tempB.size());
+    for (int i = 0; i < tempA.size(); i++){
+      a.add(tempA.get(i));
     }
-    for (int i = 0; i < b.size(); i++){
-      returnArray.add(b.get(i));
+    for (int i = 0; i < tempB.size(); i++){
+      b.add(tempB.get(i));
     }
 
-    return returnArray;
-  }
+    //return a new ArrayList that has all values of a and b in alternating order that is:
+    //a[0], b[0], a[1], b[1]...
+    //If one list is longer than the other, just attach the remaining values to the end.
+    ArrayList<String> returnArray = new ArrayList<String> (a.size() + b.size());
+    if (a.size() >= b.size()){
+      for (int i = 0; i < b.size(); i++){
+        returnArray.add(a.get(0));
+        returnArray.add(b.get(i));
+        a.remove(0);
+      }
+      for (int i = 0; i < a.size(); i++){
+        returnArray.add(a.get(i));
+      }
+
+      return returnArray;
+    }
+    else{
+      for (int i = 0; i < a.size(); i++){
+        returnArray.add(a.get(i));
+        returnArray.add(b.get(0));
+        b.remove(0);
+      }
+      for (int i = 0; i < b.size(); i++){
+        returnArray.add(b.get(i));
+      }
+
+      return returnArray;
+    }
 
 
   }
--- 2024-11-14-ArrayList/Driver.java
+++ 2024-11-14-ArrayList/Driver.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class Driver{
   public static void main(String[] args){
     ArrayList<String> randomArray = ArrayListPractice.createRandomArray(100);
-    //System.out.println(randomArray);
+    System.out.println(randomArray);
     ArrayListPractice.replaceEmpty(randomArray);
-    //System.out.println(ArrayListPractice.makeReversedList(randomArray));
+    System.out.println(ArrayListPractice.makeReversedList(randomArray));
     ArrayList<String> a = new ArrayList<String>(3);
     a.add("1");
     a.add("3");
@@ -13,6 +13,16 @@ public class Driver{
     ArrayList<String> b = new ArrayList<String>(2);
     b.add("2");
     b.add("4");
+
+    System.out.println(ArrayListPractice.mixLists(a, b));
+    System.out.println(ArrayListPractice.mixLists(b, a));
+
+    b.add("6");
+
+    System.out.println(ArrayListPractice.mixLists(a, b));
+    System.out.println(ArrayListPractice.mixLists(b, a));
+
+    b.add("8");
 
     System.out.println(ArrayListPractice.mixLists(a, b));
     System.out.println(ArrayListPractice.mixLists(b, a));
