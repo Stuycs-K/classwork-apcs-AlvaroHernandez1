d9aa46bd3396e3694ea67e52da94c3a00478ccbc
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-18 11:22:12 -0500

    Set UP

---
+++ 2024-11-18-advent2016/Day1.java
@@ -0,0 +1,38 @@
+//1 you need all three imports.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day1 {
+  public static void main(String[] args) {
+    System.out.println(countTrianglesA("inputTriA.txt"));
+    System.out.println(countTrianglesB("inputTri.txt"));
+  }
+
+  public static int blocksTo(String filename){
+    //2 Opening a file requires a try/catch
+    try {
+      File file = new File(filename);//1
+      Scanner input = new Scanner(file);
+      //CODE THAT SCANS THE FILE.
+      int count = 0;
+      while (input.hasNextInt()){
+        int s1 = input.nextInt();
+        int s2 = input.nextInt();
+        int s3 = input.nextInt();
+
+        if (isTriangle(s1, s2, s3))
+          count ++;
+
+      }
+
+      input.close();//releases the file from your program
+      return count;
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return 0; //you can return from a void function just don't put a value.
+    }
+  }
+}
