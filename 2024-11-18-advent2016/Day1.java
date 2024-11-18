3d5e53fefca8eee100ccec7807cc6fec366c7aa4
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-18 18:10:29 -0500

    Fixed Day1 code for part A and started working on part B

--- 2024-11-18-advent2016/Day1.java
+++ 2024-11-18-advent2016/Day1.java
@@ -8,19 +8,17 @@ public class Day1 {
     System.out.println(blocksTo("input.txt"));
   }
 
-∫  public static int blocksTo(String filename){
+  public static int blocksTo(String filename){
     //2 Opening a file requires a try/catch
     try {
       File file = new File(filename);//1
       Scanner input = new Scanner(file);
       //CODE THAT SCANS THE FILE.
       String total = input.nextLine();
-      int x = 0;
-      int y = 0;
       int direction = 0; // 0 = north, 1 = east, 2 = south, 3 = west
-      int distanceTo = 0;
       String[] directions = total.split(", ");
-      System.out.println(directions.length);
+      int[] x = new int[directions.length];
+      int[] y = new int[directions.length];
       for (int i = 0; i < directions.length; i++){
         if (directions[i].substring(0,1).equals("R")){
           if (direction < 3)
@@ -34,22 +32,19 @@ public class Day1 {
           else
             direction = 3;
         }
-        System.out.println(Integer.parseInt("123"));
-        /*
         if (direction == 1)
-          x += parseInt(directions[i].substring(1));
+          x += Integer.parseInt(directions[i].substring(1));
         if (direction == 2)
-          y -= parseInt(directions[i].substring(1));
+          y -= Integer.parseInt(directions[i].substring(1));
         if (direction == 3)
-          x -= parseInt(directions[i].substring(1));
+          x -= Integer.parseInt(directions[i].substring(1));
         if (direction == 0)
-          y += parseInt(directions[i].substring(1));
-        */
+          y += Integer.parseInt(directions[i].substring(1));
       }
 
 
       input.close();//releases the file from your program
-      return distanceTo;
+      return Math.abs(x) + Math.abs(y);
 
     } catch (FileNotFoundException ex) {
       //File not found what should you do?
