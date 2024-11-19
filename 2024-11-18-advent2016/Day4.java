e4e06729669303a6fdfeb63b67fec6442989a94d
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-19 00:32:34 -0500

    Halfway fone with 4B

--- 2024-11-18-advent2016/Day4.java
+++ 2024-11-18-advent2016/Day4.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Day3 {
+public class Day4 {
   public static void main(String[] args) {
     System.out.println(code("input.txt"));
   }
