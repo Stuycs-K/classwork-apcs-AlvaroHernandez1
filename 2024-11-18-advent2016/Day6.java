f6c47db394bbb068622c28201855534e4d2df4a1
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-19 14:47:04 -0500

    Changed file name

--- 2024-11-18-advent2016/Day6.java
+++ 2024-11-18-advent2016/Day6.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Day6A {
+public class Day6 {
   public static void main(String[] args) {
     System.out.println(code("input.txt"));
   }
