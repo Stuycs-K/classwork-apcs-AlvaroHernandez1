f4fdd084f83fce99a1100808ee3a02b98a14d965
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-19 11:46:30 -0500

    Coded day 6A, at the point where I have individual Strings of all the letters in each column

--- 2024-11-18-advent2016/Day6A.java
+++ 2024-11-18-advent2016/Day6A.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Day4B {
+public class Day6A {
   public static void main(String[] args) {
     System.out.println(code("input.txt"));
   }
@@ -13,12 +13,32 @@ public class Day4B {
       File file = new File(filename);//1
       Scanner input = new Scanner(file);
       //CODE THAT SCANS THE FILE.
-      while (input.hasNextLine()){
+      String column0 = "";
+      String column1 = "";
+      String column2 = "";
+      String column3 = "";
+      String column4 = "";
+      String column5 = "";
+      String column6 = "";
+      String column7 = "";
+
+      while (input.hasNext()){
+        String line = input.next();
+
+        column0 += line.charAt(0);
+        char c1 = line.charAt(1);
+        char c2 = line.charAt(2);
+        char c3 = line.charAt(3);
+        char c4 = line.charAt(4);
+        char c5 = line.charAt(5);
+        char c6 = line.charAt(6);
+        char c7 = line.charAt(7);
+
 
 
       }
       input.close();//releases the file from your program
-
+      System.out.println(column0);
       return 0;
 
     } catch (FileNotFoundException ex) {
