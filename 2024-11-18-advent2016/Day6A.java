b9fcb46882edde3702079a083944d1768c20d1aa
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-11-19 11:31:19 -0500

    Set up for Day 6

---
+++ 2024-11-18-advent2016/Day6A.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Day4B {
+  public static void main(String[] args) {
+    System.out.println(code("input.txt"));
+  }
+
+  public static int code(String filename){
+    try {
+      File file = new File(filename);//1
+      Scanner input = new Scanner(file);
+      //CODE THAT SCANS THE FILE.
+      while (input.hasNextLine()){
+
+
+      }
+      input.close();//releases the file from your program
+
+      return 0;
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");
+      return 0; //you can return from a void function just don't put a value.
+    }
+  }
+}
