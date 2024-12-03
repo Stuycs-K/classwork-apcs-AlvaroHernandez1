9e44f9226c3f4328d34cced59e14216d23ece444
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-12-03 11:57:58 -0500

    started art stuff cus im an artist

--- 2024-12-03-colors/test.java
+++ 2024-12-03-colors/test.java
@@ -1,8 +1,17 @@
 public class test{
   public static void main(String[] args){
-    System.out.print("\u001b[31mRed");
-    System.out.print(" \u001b[0mfish ");
-    System.out.print("\u001b[1mBlue");
-    System.out.print(" \u001b[0mfish ");
+    int g = 0;
+    int b = 0;
+    for (int i = 20; i > 0; i--){
+      for(int r = 0; r < 252; r+=8){
+        System.out.print("\u001b[48;2;"+r+";"+g+";"+b+";7m");
+        System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+      }
+      for(int r = 252; r > 0; r-=8){
+        System.out.print("\u001b[48;2;"+r+";"+g+";"+b+";7m");
+        System.out.print("\u001b[38;2;"+r+";"+g+";"+b+";7m.");
+      }
+    }
+    System.out.println("\u001b[0m");
   }
 }
