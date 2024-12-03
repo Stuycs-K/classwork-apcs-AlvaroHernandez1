1d4ba9553b2918e74610a357ad282bbb36ebe6a7
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-12-03 11:38:14 -0500

    added directory for classwork - colors

---
+++ 2024-12-03-colors/test.java
@@ -0,0 +1,8 @@
+public class test{
+  public static void main(String[] args){
+    System.out.print("\u001b[31mRed");
+    System.out.print(" \u001b[0mfish ");
+    System.out.print("\u001b[1mBlue");
+    System.out.print(" \u001b[0mfish ");
+  }
+}
