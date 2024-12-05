496e9f702f46837785172102be0544c17c33a1b6
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-12-04 19:20:24 -0500

    Have 2 steps left

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -19,6 +19,53 @@ public class Driver{
       Text.go(row, 80);
       System.out.print(" ");
     }
+    Text.color(Text.YELLOW);
+    for (int col = 2; col < 80; col++){
+      Text.go(3, col);
+      System.out.print("-");
+    }
+
+    //extra
+    int column = 15;
+    for (int row = 6; row < 28; row++){
+      Text.go(row, column);
+      Text.color(Text.RED, Text.background(Text.RED));
+      System.out.print(" ");
+    }
+    column = 35;
+    for (int row = 6; row < 28; row++){
+      Text.go(row, column);
+      Text.color(Text.RED, Text.background(Text.RED));
+      System.out.print(" ");
+    }
+    column = 60;
+    for (int row = 6; row < 28; row++){
+      Text.go(row, column);
+      Text.color(Text.RED, Text.background(Text.RED));
+      System.out.print(" ");
+    }
+
+    int row = 6;
+    for (int col = 55; col < 66; col++){
+      Text.go(row, col);
+      Text.color(Text.RED, Text.background(Text.RED));
+      System.out.print(" ");
+    }
+    row = 27;
+    for (int col = 55; col < 66; col++){
+      Text.go(row, col);
+      Text.color(Text.RED, Text.background(Text.RED));
+      System.out.print(" ");
+    }
+    row = 17;
+    for (int col = 15; col < 35; col++){
+      Text.go(row, col);
+      Text.color(Text.RED, Text.background(Text.RED));
+      System.out.print(" ");
+    }
+
+
+    Text.go(30, 80);
     System.out.println(Text.RESET);
   }
   
