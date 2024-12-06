54a49c54c15da83db1536ce8dbe40fc65aa595f8
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-12-06 11:29:22 -0500

    Added files for classwork and began work on berserk

--- 2024-12-04-ColorScreen/Driver.java
+++ 2024-12-04-ColorScreen/Driver.java
@@ -2,7 +2,7 @@ public class Driver{
   public static void main(String[] args){
     System.out.print(Text.CLEAR_SCREEN);
     System.out.print(Text.HIDE_CURSOR);
-    System.out.print(Text.color(YELLOW));
+    System.out.print(Text.color("BLUE"));
     System.out.println("Yellow");
     System.out.println(Text.RESET);
   }
