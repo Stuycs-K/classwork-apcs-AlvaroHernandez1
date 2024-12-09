4fb42ff0357c35beb1b0a9439b54a42b372b64cf
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2024-12-09 11:46:27 -0500

    Added if statements for actions started fixing quiting functionality

--- 2024-12-06-Adventurer/Main.java
+++ 2024-12-06-Adventurer/Main.java
@@ -9,8 +9,29 @@ class Main {
     Adventurer p1 = new Berserk("Jeff");
     System.out.println(p1.getName() + ", " + p1.getHP() + "/" + p1.getmaxHP() + ", " + p1.getSpecial() + "/" + p1.getSpecialMax() + " " + p1.getSpecialName());
 
+    Adventurer p2 = new CodeWarrior();
+    System.out.println(p2.getName() + ", " + p2.getHP() + "/" + p2.getmaxHP() + ", " + p2.getSpecial() + "/" + p2.getSpecialMax() + " " + p2.getSpecialName());
 
-
+    //Asks fpr p1 action
+    boolean quit = false;
+    while (p1.getHP() > 0 && p2.getHP() > 0 && quit == false){
+      System.out.print("Enter Action a)ttack / (sp)ecial / (su)pport / quit: ");
+      String action = userInput.nextLine();
+      if (action.equals("attack") || action.equals("a")){
+        p1.attack(p2);
+      }
+      if (action.equals("special") || action.equals("sp")){
+        p1.specialAttack(p2);
+      }
+      if (action.equals("support") || action.equals("su")){
+        p1.support();
+        p1.support(p2);
+      }
+      if (action.equals("quit")){
+        throw new IllegalArgumentException(
+          "Player Quit Game...");
+      }
+    }
 
 
 
