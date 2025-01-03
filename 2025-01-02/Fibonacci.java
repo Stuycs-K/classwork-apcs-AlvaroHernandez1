877b83ed683812225e9327bc82253fe146ad7990
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2025-01-03 11:35:55 -0500

    Tested and it works and changed file name

--- 2025-01-02/Fibonacci.java
+++ 2025-01-02/Fibonacci.java
@@ -2,7 +2,7 @@ public class Fibonacci{
   public static void main(String[] args){
     System.out.println(fib(5));
   }
-  public int fib(int n) {
+  public static int fib(int n) {
     if (n > 1){
       return fib(n-2) + fib(n-1);
     }
